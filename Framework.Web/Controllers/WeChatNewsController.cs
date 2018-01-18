6404fc16926a684b848d26a69a45ea09cf3d6eb8
Administrator <Administrator@MS-20170511IAEE>
2018-01-18 17:16:38 +0800

    123456

---
+++ Framework.Web/Controllers/WeChatNewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Framework.Web.Controllers
+{
+    /// <summary>
+    /// 图文素材列表
+    /// </summary>
+    public class WeChatNewsController : Controller
+    {
+        //
+        // GET: /WeChatNews/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
